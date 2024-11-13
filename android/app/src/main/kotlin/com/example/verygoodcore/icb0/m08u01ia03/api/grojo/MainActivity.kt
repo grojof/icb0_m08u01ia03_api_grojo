60b339e20650ab2c5b66c607f68d37fb9618a0a6
Guillermo Rojo <47704716+grojof@users.noreply.github.com>
2024-11-13 17:52:09 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/icb0/m08u01ia03/api/grojo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.icb0_m08u01ia03_api_grojo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:icb0_m08u01ia03_api_grojo/app/app.dart';
+import 'package:icb0_m08u01ia03_api_grojo/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
